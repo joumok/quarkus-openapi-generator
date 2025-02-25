17c6b82809e05a687b48a39e6fe8021951d9d4ae
joao <joaovitor.rt2001@uel.br>
2025-02-25 10:42:57 -0300

    fix: modifiying the qutetemplateengineadapter

--- client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapter.java
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapter.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.openapitools.codegen.api.AbstractTemplatingEngineAdapter;
 import org.openapitools.codegen.api.TemplatingExecutor;
 
@@ -20,8 +21,6 @@ import io.quarkus.qute.Template;
 public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter {
 
     public static final String IDENTIFIER = "qute";
-    public static final String TEMPLATE_DIRECTORY = "src/main/resources/templates";
-
     public static final String[] INCLUDE_TEMPLATES = {
             "additionalEnumTypeAnnotations.qute",
             "additionalEnumTypeUnexpectedMember.qute",
@@ -45,7 +44,9 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
             "pojoAdditionalProperties.qute",
             "operationJavaDoc.qute"
     };
-
+    public static final String TEMPLATE_DIRECTORY = ConfigProvider.getConfig()
+            .getOptionalValue("quarkus.openapi-generator.codegen.template-base-dir", String.class)
+            .orElse("src/main/resources/templates");
     public final Engine engine;
     private final List<String> includeTemplates;
 
@@ -59,7 +60,7 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
                 .strictRendering(true)
                 .build();
 
-        this.includeTemplates = loadTemplateFiles(); // Carrega arquivos dinamicamente
+        this.includeTemplates = loadTemplateFiles();
     }
 
     @Override
@@ -75,7 +76,7 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
     @Override
     public String compileTemplate(TemplatingExecutor executor, Map<String, Object> bundle, String templateFile)
             throws IOException {
-        cacheTemplates(executor);
+        this.cacheTemplates(executor);
         Template template = engine.getTemplate(templateFile);
         if (template == null) {
             template = engine.parse(executor.getFullTemplateContents(templateFile));
@@ -85,7 +86,7 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
     }
 
     public void cacheTemplates(TemplatingExecutor executor) {
-        for (String templateId : includeTemplates) {
+        for (String templateId : INCLUDE_TEMPLATES) {
             Template incTemplate = engine.getTemplate(templateId);
             if (incTemplate == null) {
                 incTemplate = engine.parse(executor.getFullTemplateContents(templateId));
@@ -94,28 +95,23 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
         }
     }
 
-    /**
-     * Tenta carregar os templates a partir do diretório definido.
-     * Se o diretório não existir ou se algum dos arquivos padrão não estiver presente,
-     * estes são adicionados à lista.
-     */
     private List<String> loadTemplateFiles() {
         Path templateDirPath = Paths.get(TEMPLATE_DIRECTORY);
         List<String> templates = new ArrayList<>();
 
         if (Files.exists(templateDirPath) && Files.isDirectory(templateDirPath)) {
-            try (Stream<Path> paths = Files.walk(templateDirPath)) {
+            try (Stream<Path> paths = Files.list(templateDirPath)) {
                 templates = paths.filter(Files::isRegularFile)
-                        // Preserva a estrutura de subpastas relativa ao diretório de templates
-                        .map(path -> templateDirPath.relativize(path).toString())
+                        .map(Path::getFileName)
+                        .map(Path::toString)
+                        .map(name -> name.replace("\\", "/"))
                         .filter(name -> name.endsWith(".qute"))
                         .collect(Collectors.toCollection(ArrayList::new));
             } catch (IOException e) {
-                throw new RuntimeException("Erro ao carregar templates do diretório: " + TEMPLATE_DIRECTORY, e);
+                throw new RuntimeException("Error while loading qute templates from: " + TEMPLATE_DIRECTORY, e);
             }
         }
 
-        // Se algum template padrão não estiver na lista, adiciona-o
         for (String defaultTemplate : INCLUDE_TEMPLATES) {
             if (!templates.contains(defaultTemplate)) {
                 templates.add(defaultTemplate);
